50e09766f6a96276224eb9680aebfbddcd0005a8
jogabi <rkql1109@naver.com>
2024-08-02 11:02:08 +0900

    page 수정 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      jojgabitest3242344242422424
+      jㅅㄷㄴㅅ
     </main>
   );
 }
