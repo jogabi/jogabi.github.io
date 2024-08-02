d5b94beb856483cba1cf1886939e642c9e2e5ce8
jogabi <rkql1109@naver.com>
2024-08-02 14:30:08 +0900

    sassOptions 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
     output: 'export',
-    // sassOptions: {
-    //   includePaths: [path.join(__dirname, 'styles')],
-    // },
+    sassOptions: {
+      includePaths: [path.join(__dirname, 'styles')],
+    },
 
 };
 
