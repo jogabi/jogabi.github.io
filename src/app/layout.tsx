60a8e0c19e453cb9e75cbe34eeb52b5f849a5987
jogabi <rkql1109@naver.com>
2024-08-02 15:20:53 +0900

    reset style leftmenu 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import LeftMenu from "@/components/LeftMenu";
+import "@/styles/style.scss";
+import "@/styles/reset.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div id="wrap">
+        <LeftMenu />
+        <div id="container">
+        {children}
+          </div>
+          </div>
+
+      </body>
     </html>
   );
 }
---
+++ src/components/LeftMenu.tsx
@@ -0,0 +1,15 @@
+// components/LeftMenu.js
+
+const LeftMenu = () => {
+    return (
+        <div className="left-menu">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
+};
+
+export default LeftMenu;
