ba1f18a4c9b18278c60749f3af3cf0f335d05831
jogabi <rkql1109@naver.com>
2024-08-02 13:48:44 +0900

    파일필터링 추가

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -7,8 +7,11 @@ const contentDirectory = path.join( process.cwd(), 'src/content' );
 
 export async function getAllMarkdownContent() {
   const fileNames = fs.readdirSync( contentDirectory );
+  const markdownFileNames = fileNames.filter( fileName => path.extname( fileName ) === '.md' );
+
+
   const allContentHtml = await Promise.all(
-    fileNames.map( async ( fileName ) => {
+    markdownFileNames.map( async ( fileName ) => {
       const fullPath = path.join( contentDirectory, fileName );
       const fileContents = fs.readFileSync( fullPath, 'utf8' ); // 인코딩을 'utf8'로 설정
       const processedContent = await remark().use( html ).process( fileContents );
