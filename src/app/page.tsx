6360ca213bb8765ef08de1142b428046b1311c59
jogabi <rkql1109@naver.com>
2024-08-02 11:10:54 +0900

    page 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import fs from 'fs';
-import path from 'path';
-import { remark } from 'remark';
-import html from 'remark-html';
+import { getMarkdownContent } from '@/lib/markdown';
 
-const contentDirectory = path.join(process.cwd(), 'src/content');
+export default async function Home() {
+  const contentHtml = await getMarkdownContent('example');
 
-export async function getMarkdownContent(fileName: string) {
-  const fullPath = path.join(contentDirectory, `${fileName}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-  const processedContent = await remark().use(html).process(fileContents);
-  const contentHtml = processedContent.toString();
-
-  return contentHtml;
+  return (
+    <main>
+      <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
+    </main>
+  );
 }
