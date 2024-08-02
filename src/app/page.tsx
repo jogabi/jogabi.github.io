70c80498e3bdbb26c409c02091d6bbcf056d80b9
jogabi <rkql1109@naver.com>
2024-08-02 11:12:40 +0900

    page 수정 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 import { getMarkdownContent } from '@/lib/markdown';
 
-export default function Home({ contentHtml }: { contentHtml: string }) {
+export default async function Home() {
+  const contentHtml = await getMarkdownContent('example');
+
   return (
     <main>
       <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
     </main>
   );
-}
-
-export async function getStaticProps() {
-  const contentHtml = await getMarkdownContent('example');
-  return {
-    props: {
-      contentHtml,
-    },
-  };
 }
