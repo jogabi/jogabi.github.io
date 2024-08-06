4808213892a6a9299a152bc750edcb8f2e8148b1
jogabi <rkql109@naver.com>
2024-08-06 16:46:43 +0900

    header 수정중

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,9 +2,15 @@ import Navigation from "@/components/Navigation";
 
 const Header = () => {
   return (
-    <div className="header_wrap">
+    <>
+      <div className="header_top_wrap">
+        <div></div>
+        <div className="logo">GABI JO</div>
+
+        <span className="information">INFORMATION</span>
+      </div>
       <Navigation />
-    </div>
+    </>
   );
 };
 
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -8,6 +8,7 @@ const Navigation = () => {
       </ul>
     </div>
   );
+  ``;
 };
 
 export default Navigation;
