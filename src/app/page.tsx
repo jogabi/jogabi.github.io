0d197879de10f3a721ec1adce7d1b80e382c221a
jogabi <rkql109@naver.com>
2024-08-08 15:44:43 +0900

    main page 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,30 @@
-import { getAllMarkdownContent } from '@/lib/markdown';
+import { getAllMarkdownContent } from "@/lib/markdown";
+import { JSDOM } from "jsdom";
 
 export default async function Home() {
   const allContentHtml = await getAllMarkdownContent();
 
   return (
     <main>
-      {allContentHtml.map((content, index) => (
-        <div key={index} dangerouslySetInnerHTML={{ __html: content }} />
-      ))}
+      {allContentHtml.map((content, index) => {
+        const dom = new JSDOM(content);
+        const doc = dom.window.document;
+        const images = doc.querySelectorAll("img");
+
+        if (images.length > 1) {
+          for (let i = 1; i < images.length; i++) {
+            images[i].remove();
+          }
+        }
+        const modifiedContent = doc.body.innerHTML;
+
+        return (
+          <div
+            key={index}
+            dangerouslySetInnerHTML={{ __html: modifiedContent }}
+          />
+        );
+      })}
     </main>
   );
 }
