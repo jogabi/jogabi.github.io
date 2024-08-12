1722a19655abe5cb44290b40cebcffbd5fbffaad
jogabi <rkql109@naver.com>
2024-08-12 15:07:20 +0900

    상세페이지 generateStaticParams .md 가져오기

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,11 +2,29 @@ import fs from "fs";
 import path from "path";
 
 export function generateStaticParams() {
-  // return files.map((file: any) => ({
-  //   id: path.basename(file, ".md"),
-  // }));
+  const contentDirectory = path.join(process.cwd(), "src/content");
+  const files = getAllMarkdownFiles(contentDirectory);
 
-  return [{ id: "2024-07-02-06" }];
+  return files.map((file: any) => ({
+    id: path.basename(file, ".md"),
+  }));
+}
+
+function getAllMarkdownFiles(dir: string): string[] {
+  let results: string[] = [];
+  const list = fs.readdirSync(dir);
+  list.forEach((file) => {
+    const filePath = path.join(dir, file);
+    const stat = fs.statSync(filePath);
+
+    if (stat && stat.isDirectory()) {
+      results = results.concat(getAllMarkdownFiles(filePath));
+    } else if (path.extname(file) === ".md") {
+      results.push(filePath);
+    }
+  });
+
+  return results;
 }
 
 export default function Page({ params }: { params: { id: string } }) {
